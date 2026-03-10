be6616b4ca81b77f1ae0ebb95d134b31f3fe099e
mufti-1448 <mufti0480@gmail.com>
2026-03-10 20:09:00 +0700

    Deleted Railway deployment files and documentation.

--- project/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Start script for environments that don't provide heroku-php-apache2
-# Uses PHP built-in server to serve the Laravel `public/` folder.
-# PORT is provided by the platform (Railway). Default to 8080.
-
-PORT=${PORT:-8080}
-php -d variables_order=EGPCS -S 0.0.0.0:${PORT} -t public/
